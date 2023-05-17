da409b8dd509c4d069322a13e9e22e98ea13cd3a
Tyler Fullerton <tyler.fullerton@vercara.com>
2023-05-17 10:02:36 -0700

    Created LogManager class in ddrLogs.py

--- ddrLogs.py
+++ ddrLogs.py
@@ -3,28 +3,37 @@ import urllib.parse
 from ddrClient import DDRClient
 from pprint import pprint
 
-def getLogs():
+class LogManager(DDRClient):
 
+    def __init__(self, apiKey, apiHost=DDRClient.hostOptions['alternate']):
+        DDRClient.__init__(self, apiKey, apiHost)
+    
+    def getLogs(self, startDateTime, endDateTime):
+    
+        # Construct data for query
+        data = {
+            "applied_filters": [{
+                "exclude": False,
+                "id": "datetime",
+                "isRange": True,
+                "rangeValue": {
+                    "end": endDateTime,
+                    "start": startDateTime
+                }
+            }]
+        }
+        
+        rsp = client.doPost('/api/protect/ext/logs', {}, data)
+        return rsp
+
+if __name__ == "__main__":
+
+    start   = "2023-04-20T01:21:58.283Z"
+    end     = "2023-04-24T01:21:58.283Z"
     apiKey  = '[TODO: API KEY HERE]'
-    client  = DDRClient(apiKey, DDRClient.hostOptions['alternate'])
-
-    data    = {
-                "applied_filters":  [
-                    {
-                        "exclude": False,
-                        "id": "datetime",
-                        "isRange": True,
-                        "rangeValue": {
-                            "end": "2023-04-24T01:21:58.283Z",
-                            "start": "2023-04-20T01:21:58.283Z"
-                        }
-                    }
-                ]
-             }
-
-    rsp = client.doPost('/api/protect/ext/logs', {}, data)
-    print("URL:", rsp.url)
+    client  = LogManager(apiKey)
     
-    pprint(rsp.json())
-    
-getLogs()
+    rsp = client.getLogs(start, end)
+
+    for log in rsp.json()['logs']:
+        print("LOG ENTRY:", log)
